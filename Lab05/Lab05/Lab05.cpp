03affa119e0c0a6e96cad45eb9da528e1c50e543
Vlad Smirnov <smirnovvlad313@gmail.com>
2023-01-29 11:51:20 +0300

    Лабораторная работа №5

---
+++ Lab05/Lab05/Lab05.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Graph.h"
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    Graph graph;
+    cout << "Список ребер остовного дерева и их вес (алгоритм Прима)" << endl;
+    graph.Prims();
+    cout << "\nСписок ребер остовного дерева и их вес (алгоритм Краскала)" << endl;
+    graph.Kruskals();
+}
