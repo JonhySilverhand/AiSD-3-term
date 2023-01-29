fcbb09b9cd7a9934be5b1c2c499fe115c5f4cf33
Vlad Smirnov <smirnovvlad313@gmail.com>
2023-01-29 11:53:51 +0300

    Лабораторная работа №10

---
+++ Lab10/Lab10/AntColony.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <locale>
+#include <stdlib.h>
+#include <iostream>
+#include <malloc.h>
+using namespace std;
+
+#define NUMBER_OF_ANTS 20
+
+struct AntWay 
+{
+	int itabu;
+	int length;
+	int* tabu;
+};
+
+int randNum(int start, int end);
+
+class AntColony
+{
+public:
+	AntWay AntColonyOptimization(int** distance0, int vertex, int start, int finish);
+	AntColony(int alpha, int beta, int q)
+	{
+		ALPHA = alpha;
+		BETA = beta;
+		TIME_OF_LIFE = q;
+	}
+private:
+	int ALPHA = 0;
+	int BETA = 0;
+	int TIME_OF_LIFE = 0;
+	const int Q = 100;
+	const int RHO = 0.5;
+	double probability(int to, AntWay ant, double** pheromone, double** distance, int vertex);
+};
+
